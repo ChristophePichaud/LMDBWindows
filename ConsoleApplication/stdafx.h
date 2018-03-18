199eaec9f8e62eaf89c42422a7dd05947890c7d9
Christophe <Christophe@DESKTOP-4DB7PC9>
2018-03-18 05:41:35 +0100

    Add files

---
+++ ConsoleApplication/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "lmdb.h"
+
+#pragma comment (lib, "LMDBWindows.lib")
+#pragma comment (lib, "ntdll.lib")
+
+// TODO: reference additional headers your program requires here
